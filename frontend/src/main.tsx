53cbca60d9cbb9bdb4989b6f6b95f43a74404de3
Lok Lok <yellowhappy831@gmail.com>
2026-08-24 18:16:42 +0800

    save

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -55,6 +55,7 @@ createRoot(document.getElementById("root")!).render(
             </ColumnLayout>
           }
         />
+
         <Route path="scenes/:sceneSlug" element={<SceneEditorPage />} />
 
         <Route
